febfa0bffe1a53ec33eb55e66894c8ae7c0a36a6
Glenn Block <glenn.block@gmail.com>
2013-03-10 01:16:33 -0800

    refactoring

--- test/WebApiBook.IssueTrackerApi.Tests/IssueSourceTests.cs
+++ test/WebApiBook.IssueTrackerApi.Tests/IssueSourceTests.cs
@@ -10,35 +10,35 @@ using WebApiContrib.Testing;
 using Xunit;
 using Should;
 
-namespace IssueTrackerApi.Tests
+namespace WebApiBook.IssueTrackerApi.Tests
 {
+    public class IssueSourceTests
+    {
+        [Fact]
+        public void ShouldCreateClientWhenConstructed()
+        {
+            var source = new TestIssueSource(null);
+            source.Client.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldSetHandlerIfPassedInWhenConstructed()
+        {
+            var handler = new FakeHandler(h => null);
+            var source = new TestIssueSource(handler);
+            var handlerField = typeof(HttpMessageInvoker).GetField("handler",
+                                                                    BindingFlags.NonPublic | BindingFlags.Instance);
+            var clientHandler = (HttpMessageHandler)handlerField.GetValue(source.Client);
+            clientHandler.ShouldEqual(handler);
+        }
+    }
+
     public class TestIssueSource : IssueSource
     {
         public TestIssueSource(HttpMessageHandler handler)
             : base(handler)
         {
 
-        }
-
-        public class IssueSourceTests
-        {
-            [Fact]
-            public void ShouldCreateClientWhenConstructed()
-            {
-                var source = new TestIssueSource(null);
-                source.Client.ShouldNotBeNull();
-            }
-
-            [Fact]
-            public void ShouldSetHandlerIfPassedInWhenConstructed()
-            {
-                var handler = new FakeHandler(h => null);
-                var source = new TestIssueSource(handler);
-                var handlerField = typeof (HttpMessageInvoker).GetField("handler",
-                                                                        BindingFlags.NonPublic | BindingFlags.Instance);
-                var clientHandler = (HttpMessageHandler) handlerField.GetValue(source.Client);
-                clientHandler.ShouldEqual(handler);
-            }
         }
     }
 }
