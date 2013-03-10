fa41dd72d8e997236868a22c067e6f934be08d8c
Glenn Block <glenn.block@gmail.com>
2013-03-10 01:18:07 -0800

    Initializing Action and Link collections

--- src/WebApiBook.IssueTrackerApi/Models/Issue.cs
+++ src/WebApiBook.IssueTrackerApi/Models/Issue.cs
@@ -7,17 +7,23 @@ namespace WebApiBook.IssueTrackerApi.Models
 {
     public class Issue
     {
+        public Issue()
+        {
+            Actions = new List<ActionLink>();
+            Links = new List<Link>();
+        }
+
         public string Id { get; set; }
 
-        public Uri Href { get; set; }
+        public string Href { get; set; }
 
         public string Title { get; set; }
         
         public string Description { get; set; }
 
-        public IEnumerable<ActionLink> Actions { get; set; }
+        public IList<ActionLink> Actions { get; set; }
 
-        public IEnumerable<Link> Links { get; set; } 
+        public IList<Link> Links { get; set; } 
 
         public string State { get; set; }
     }
