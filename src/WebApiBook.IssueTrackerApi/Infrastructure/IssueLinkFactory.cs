c7ab1d927ad8cca8c6056e626ef99deb8a102cbc
Glenn Block <glenn.block@gmail.com>
2014-05-22 03:24:07 -0700

    Enabling CORS, also fixing RELs to be unique for actions

--- src/WebApiBook.IssueTrackerApi/Infrastructure/IssueLinkFactory.cs
+++ src/WebApiBook.IssueTrackerApi/Infrastructure/IssueLinkFactory.cs
@@ -9,7 +9,10 @@ namespace WebApiBook.IssueTrackerApi.Infrastructure
         private const string Prefix = "http://webapibook.net/profile#";
         
         public new class Rels : LinkFactory.Rels {
-            public const string IssueProcessor = Prefix + "issue-processor";
+            public const string IssueProcessor = Prefix + "issue-processor-";
+            public const string Open = Prefix + Actions.Open;
+            public const string Close = Prefix + Actions.Close;
+            public const string Transition = Prefix + Actions.Transition;
             public const string SearchQuery = Prefix + "search";
         }
         
@@ -26,17 +29,17 @@ namespace WebApiBook.IssueTrackerApi.Infrastructure
    
         public Link Transition(string id)
         {
-            return GetLink<IssueProcessorController>(Rels.IssueProcessor, id, Actions.Transition);
+            return GetLink<IssueProcessorController>(Rels.Transition, id, Actions.Transition);
         }
         
         public Link Open(string id)
         {
-            return GetLink<IssueProcessorController>(Rels.IssueProcessor, id, Actions.Open);
+            return GetLink<IssueProcessorController>(Rels.Open, id, Actions.Open);
         }
         
         public Link Close(string id)
         {
-            return GetLink<IssueProcessorController>(Rels.IssueProcessor, id, Actions.Close);
+            return GetLink<IssueProcessorController>(Rels.Close, id, Actions.Close);
         }
     }
 }
