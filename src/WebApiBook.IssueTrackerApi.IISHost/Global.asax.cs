99c4daec81e711ab637fe99182a082c83f3d7dad
Glenn Block <glenn.block@gmail.com>
2014-07-07 01:35:40 -0700

    Adding IIS host

---
+++ src/WebApiBook.IssueTrackerApi.IISHost/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApiBook.IssueTrackerApi.IISHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            WebApiConfiguration.Configure(GlobalConfiguration.Configuration);
+        }
+    }
+}
