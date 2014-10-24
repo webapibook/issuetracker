d3cced7aa8ab5b75165e0e2b25d549bd14809c73
Darrel <darrel@tavis.ca>
2014-10-24 17:34:09 -0400

    Added acceptance tests for Home Controller

---
+++ test/WebApiBook.IssueTrackerApi.AcceptanceTests/HomeFeature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApiBook.IssueTrackerApp.AcceptanceTests
+{
+    public abstract class HomeFeature
+    {
+        public HttpClient Client;
+        public HttpRequestMessage Request;
+        public HttpResponseMessage Response;
+
+        public HomeFeature()
+        {
+            Request = new HttpRequestMessage();
+
+            var config = new HttpConfiguration();
+            WebApiBook.IssueTrackerApi.WebApiConfiguration.Configure(config);
+            var server = new HttpServer(config);
+            Client = new HttpClient(server);
+        }
+
+
+    }
+}
