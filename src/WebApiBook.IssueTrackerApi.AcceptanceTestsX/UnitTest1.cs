5c294eb60a51f829cf9f7353f1e315dec98e279d
Greg <greg.hoover@sungardps.com>
2015-04-04 14:30:43 -0400

    - Added XUnit Runner NuGet. XUnit tests now run. - Need to remove stuff manually added by hand to the project trying to get it to discover/run XUnit tests.

---
+++ src/WebApiBook.IssueTrackerApi.AcceptanceTestsX/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace WebApiBook.IssueTrackerApi.AcceptanceTestsX
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestMethod1()
+		{
+		}
+	}
+}
